1c2b28f089bf9a195f1ddc54daaa98d8725b01c6
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2023-05-29 17:41:21 -0300

    Errata colisões.

--- code/level.py
+++ code/level.py
@@ -16,9 +16,10 @@ class Level:
 		# Sprite groups
 		self.all_sprites = CameraGroup()
 		self.collision_sprites = pygame.sprite.Group()
+		self.tree_sprites = pygame.sprite.Group()
 		self.setup()
 		self.overlay = Overlay(self.player)
-		self.tree_sprites = pygame.sprite.Group()
+		
 	def setup(self):
 		tmx_data = load_pygame('../data/map.tmx')
 
@@ -84,17 +85,18 @@ class CameraGroup(pygame.sprite.Group):
 	def customize_draw(self, player):
 		self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
 		self.offset.y = player.rect.centery  - SCREEN_HEIGHT / 2
+		
 		for number in LAYERS.values():
-			for sprite in self.sprites():
+			for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
 				if sprite.z == number:
 					offset_rect = sprite.rect.copy()
 					offset_rect.center -= self.offset
 					self.display_surface.blit(sprite.image, offset_rect)
 
-					if sprite == player:
-						pygame.draw.rect(self.display_surface, 'red', offset_rect, 5)
-						hitbox_rect = player.hitbox.copy()
-						hitbox_rect.center = offset_rect.center
-						pygame.draw.rect(self.display_surface, 'green', hitbox_rect, 5)
-						target_pos = offset_rect.center + PLAYER_TOOL_OFFSET[player.status.split('_')[0]]
-						pygame.draw.circle(self.display_surface, 'blue', target_pos, 5)
+					# if sprite == player:
+					# 	pygame.draw.rect(self.display_surface, 'red', offset_rect, 5)
+					# 	hitbox_rect = player.hitbox.copy()
+					# 	hitbox_rect.center = offset_rect.center
+					# 	pygame.draw.rect(self.display_surface, 'green', hitbox_rect, 5)
+					# 	target_pos = offset_rect.center + PLAYER_TOOL_OFFSET[player.status.split('_')[0]]
+					# 	pygame.draw.circle(self.display_surface, 'blue', target_pos, 5)
