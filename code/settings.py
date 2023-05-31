67d1395b816190671efc91670d884773a611cf49
joaocavf <130995350+joaocavf@users.noreply.github.com>
2023-05-30 21:00:30 -0300

    Ajuste tamanho

--- code/settings.py
+++ code/settings.py
@@ -7,7 +7,7 @@ TILE_SIZE = 64
 # Overlay positions 
 OVERLAY_POSITIONS = {
 	'tool' : (40, SCREEN_HEIGHT - 15), 
-	'seed': (70, SCREEN_HEIGHT - 5)}
+	'seed': (79, SCREEN_HEIGHT - 5)}
 
 PLAYER_TOOL_OFFSET = {
 	'left': Vector2(-50,40),
